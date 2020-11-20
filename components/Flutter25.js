6f3973bbe6c440290a2caa7e6b8641d8aefab2c6
Ben Carlson <bjcarlson42@outlook.com>
2020-11-20 16:52:10 -0500

    Make 25 Days Of Flutter box clickable

--- components/Flutter25.js
+++ components/Flutter25.js
@@ -5,7 +5,8 @@ import {
     Text,
     Heading,
     Image,
-    Flex
+    Flex,
+    Link
 } from "@chakra-ui/core"
 
 const Flutter25 = () => {
@@ -16,35 +17,45 @@ const Flutter25 = () => {
         dark: 'gray.400'
     }
     return (
-        <Box
-            w="100%"
-            p={5}
+        <Link
+            href='https://flutter25.com'
+            title='25 Days Of Flutter'
+            isExternal
+            _hover={{
+                boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+                textDecoration: 'none'
+            }}
             my={4}
-            border='1px solid'
-            borderColor="lightgray"
-            borderRadius={5}
-            h="100%"
         >
-            <Flex>
-                <Image size="75px" borderRadius={5} src="/images/flutter25.png"></Image>
-                <Flex flexDirection={['column', 'row']}>
-                    <Flex
-                        width="100%"
-                        align="flex-start"
-                        justifyContent="space-between"
-                        flexDirection="column"
-                        mx={2}
-                    >
-                        <Heading as="h3" size="md">
-                            25 Days Of Flutter
+            <Box
+                w="100%"
+                p={5}
+                border='1px solid'
+                borderColor="lightgray"
+                borderRadius={5}
+                h="100%"
+            >
+                <Flex>
+                    <Image size="75px" borderRadius={5} src="/images/flutter25.png"></Image>
+                    <Flex flexDirection={['column', 'row']}>
+                        <Flex
+                            width="100%"
+                            align="flex-start"
+                            justifyContent="space-between"
+                            flexDirection="column"
+                            mx={2}
+                        >
+                            <Heading as="h3" size="md">
+                                25 Days Of Flutter
                     </Heading>
-                        <Text color={colorSecondary[colorMode]}>
-                            25 Days Of Flutter is a 25 day tutorial series designed to take you from a Flutter beginner to coding a complete app using various modern technologies.
+                            <Text color={colorSecondary[colorMode]}>
+                                25 Days Of Flutter is a 25 day tutorial series designed to take you from a Flutter beginner to coding a complete app using various modern technologies.
                     </Text>
+                        </Flex>
                     </Flex>
                 </Flex>
-            </Flex>
-        </Box>
+            </Box>
+        </Link>
     )
 }
 
