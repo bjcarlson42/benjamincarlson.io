e9cc05e6de9d6b18bf5d2bf75307d80d4c3df726
Ben Carlson <bjcarlson42@outlook.com>
2020-12-13 20:54:02 -0500

    Switch from hours to minutes.

--- components/StravaData.js
+++ components/StravaData.js
@@ -13,7 +13,7 @@ export const GitHubData = () => {
     const movingTime = data?.movingTime
 
     // conversions
-    const timeConv = (movingTime * 0.000277778).toFixed(0) // convert from seconds to hours
+    const timeConv = (movingTime * 0.0166667).toFixed(0) // given in seconds so we need to convert to minutes
     const distanceConv = (distance * 0.000621371).toFixed(0) //given in meters so we need to convert to miles
     return (
         <>
@@ -29,7 +29,7 @@ export const GitHubData = () => {
             />
             <StatBox
                 title={`${movingTime ? format(timeConv) : '–––'}`}
-                desc="Total time (hours)"
+                desc="Total time (minutes)"
                 url="https://www.strava.com/athletes/8696836"
             />
         </>
