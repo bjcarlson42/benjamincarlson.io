f822898463805373952d86832a6aae0ade06f0ee
Ben Carlson <bjcarlson42@outlook.com>
2021-03-01 12:09:47 -0500

    React comments.

--- components/blog/Comments.js
+++ components/blog/Comments.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react"
 import { useColorMode } from '@chakra-ui/react'
+import React, { Component } from "react";
+export default class Comments extends Component {
 
-export default function Comments() {
-    const { colorMode } = useColorMode()
-    const commentsTheme = {
-        light: 'github-light',
-        dark: 'github-dark'
+    constructor(props) {
+        super(props);
+        this.commentBox = React.createRef();
     }
-    return (
-        <>
-            <script
-                src="https://utteranc.es/client.js"
-                repo="bjcarlson42/comments-benjamincarlson.io"
-                issue-term="url"
-                theme={commentsTheme[colorMode]}
-                crossorigin="anonymous"
-                async
-            >
-            </script>
-        </>
-    )
+    
+    componentDidMount() {
+        // const { colorMode } = useColorMode()
+        // const utteranceTheme = colorMode === "dark" ? "github-dark" : "github-light";
+        let scriptEl = document.createElement("script");
+        scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+        scriptEl.setAttribute("crossorigin", "anonymous");
+        scriptEl.setAttribute("async", true);
+        scriptEl.setAttribute("repo", "bjcarlson42/comments-benjamincarlson.io");
+        scriptEl.setAttribute("issue-term", "url");
+        // scriptEl.setAttribute("theme", utteranceTheme);
+        scriptEl.setAttribute("theme", 'github-light');
+        this.commentBox.current.appendChild(scriptEl);
+    }
+
+    render() {
+        return (
+            <div ref={this.commentBox}/>
+        );
+    }
 }
