1b7ccb69273e1b69ca33340e54afaa506fbfc874
Ben Carlson <bjcarlson42@outlook.com>
2021-05-05 13:42:11 -0400

    fix: Utterances changes.

--- components/blog/Comments.js
+++ components/blog/Comments.js
@@ -8,15 +8,15 @@ export default class Comments extends Component {
     }
 
     componentDidMount() {
-        // const utteranceTheme = colorMode === "dark" ? "github-dark" : "github-light";
+        let colorMode = localStorage.getItem('chakra-ui-color-mode');
+        const utteranceTheme = colorMode === "dark" ? "github-dark" : "github-light";
         let scriptEl = document.createElement("script");
         scriptEl.setAttribute("src", "https://utteranc.es/client.js");
         scriptEl.setAttribute("crossorigin", "anonymous");
         scriptEl.setAttribute("async", true);
         scriptEl.setAttribute("repo", "bjcarlson42/comments-benjamincarlson.io");
-        scriptEl.setAttribute("issue-term", "url");
-        // scriptEl.setAttribute("theme", utteranceTheme);
-        scriptEl.setAttribute("theme", 'github-light');
+        scriptEl.setAttribute("issue-term", "og:title");
+        scriptEl.setAttribute("theme", utteranceTheme);
         this.commentBox.current.appendChild(scriptEl);
     }
 
