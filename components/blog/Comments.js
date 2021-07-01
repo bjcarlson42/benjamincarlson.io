dd054ee2bb1a7b6117b885a1119a8db642e9b32e
Benjamin Carlson <benjamincarlson@my.ccsu.edu>
2021-07-01 15:20:09 -0400

    update: Switch to giscus comments.

--- components/blog/Comments.js
+++ components/blog/Comments.js
@@ -8,15 +8,17 @@ export default class Comments extends Component {
     }
 
     componentDidMount() {
-        let colorMode = localStorage.getItem('chakra-ui-color-mode');
-        const utteranceTheme = colorMode === "dark" ? "github-dark" : "github-light";
         let scriptEl = document.createElement("script");
-        scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+        scriptEl.setAttribute("src", "https://giscus.app/client.js");
+        scriptEl.setAttribute("data-repo", "bjcarlson42/benjamincarlson.io");
+        scriptEl.setAttribute("data-repo-id", "MDEwOlJlcG9zaXRvcnkyNzI4MTAyMzA");
+        scriptEl.setAttribute("data-category", "General");
+        scriptEl.setAttribute("data-category-id", "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyMDA3MjQ5");
+        scriptEl.setAttribute("data-mapping", "title");
+        scriptEl.setAttribute("data-reactions-enabled", "1");
+        scriptEl.setAttribute("data-theme", "preferred_color_scheme");
         scriptEl.setAttribute("crossorigin", "anonymous");
         scriptEl.setAttribute("async", true);
-        scriptEl.setAttribute("repo", "bjcarlson42/comments-benjamincarlson.io");
-        scriptEl.setAttribute("issue-term", "og:title");
-        scriptEl.setAttribute("theme", utteranceTheme);
         this.commentBox.current.appendChild(scriptEl);
     }
 
